5d410f50c2194c540483f0f18dba3e7f6bb84636
zeminem <zeminem@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-11 14:15:26 +0000

    git-svn-id: https://plugins.svn.wordpress.org/endora/trunk@503643 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/reklama-js.js
@@ -0,0 +1,9 @@
+﻿function scroll() {
+	if(self != top) {
+		var div = document.getElementById("reklama-wplugin");
+		var rect = div.getBoundingClientRect();
+		window.scrollBy(0, rect.top - 50);
+	} else {
+		/* Nic nedělá, není totiž v IFRAME */
+	}
+}
